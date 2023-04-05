e1c3769659fe84fc4e991202e8017074cdfda668
maks0nch1k <maksimf647@gmail.com>
2023-04-05 20:33:25 +0300

    auto_ans

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
 @app.route("/<title>")
 @app.route("/index/<title>")
 def ready(title):
-    return render_template("base1.html", title=title)
+    return render_template("base.html", title=title)
 
 
 @app.route("/list_prof/<param>")
@@ -30,6 +29,21 @@ def jobs_list(param):
             "пилот дронов"]
 
     return render_template("list_prof.html", jobs=jobs, param=param)
+
+
+@app.route("/answer")
+@app.route("/auto_answer")
+def auto_ans():
+    values = {"title": "Анкета",
+              "surname": "Watny",
+              "name": "Mark",
+              "education": "выше среднего",
+              "profession": "штурман марсохода",
+              "sex": "male",
+              "motivation": "Всегда мечтал застрять на марсе!",
+              "ready": True}
+
+    return render_template("auto_answer.html", **values)
 
 
 if __name__ == '__main__':
