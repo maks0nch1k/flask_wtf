d87aa94d719ce96d1a481172337a01a00f881098
maks0nch1k <maksimf647@gmail.com>
2023-04-05 19:16:00 +0300

    list of jobs

--- main.py
+++ main.py
@@ -10,5 +10,11 @@ def ready(title):
     return render_template("base1.html", title=title)
 
 
+@app.route("/list_prof/<param>")
+def jobs_list(param):
+    jobs = ["dsadas", "asdadasda", "asdasdweedsad", "rrrr"]
+    return render_template("list_prof.html", jobs=jobs, param=param)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
